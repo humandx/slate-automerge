234095fd8d83fa9fac4300470cf0e837a5e7a4f6
Victor Shia <victor@humandx.org>
2018-06-10 10:59:14 -0700

    Write automergeJsontoSlate

--- src/utils/automergeJsonToSlate.js
+++ src/utils/automergeJsonToSlate.js
@@ -5,20 +5,43 @@
  * https://github.com/ianstormtaylor/slate/issues/1813 is completed.
  */
 
+const getLeaves = (characterList) => {
+    let leaves = [];
+    let text = ""
+    characterList.forEach((character) => {
+        text = text.concat(character.text)
+    })
+    let leaf = {
+        object: "leaf",
+        marks: [],
+        text: text
+    }
+    leaves.push(leaf)
+    return leaves
+}
 
-const fromJSON = (automergeJson) => {
+const fromJSON = (value) => {
     if (value === undefined || value === null) {
         return null;
     }
+
+    let newJson = {};
 
     let object = value.object;
     switch(object) {
         case "text":
             // Difficult conversion. Look in
             // slate/packages/slate/src/models/text.js -> toJSON() and getLeaves()
+            newJson = value;
+            newJson.leaves = getLeaves(value.characters)
             break;
-        default;
-
+        default:
+            newJson = value;
+            newJson.nodes = value.nodes.map((node) => {return fromJSON(node)})
             break;
     }
+
+    return newJson;
 }
+
+export default fromJSON
