7ea10890a3fcbcfef3255461f8d6e49aabcaa40f
Victor Shia <victor@humandx.org>
2018-06-12 13:10:32 -0700

    Update to latest Automerge
    Keeping install_packages in case we need custom packages in the future

--- install_packages.sh
+++ install_packages.sh
@@ -1,19 +1,20 @@
 # Clear old packages
-rm -Rf node_modules/ tmp/ yarn.lock
+rm -Rf node_modules/ yarn.lock
+# rm -Rf node_modules/ tmp/ yarn.lock
 
 # Make tmp directory for custom git repos
-mkdir -p tmp
-cd tmp
+# mkdir -p tmp
+# cd tmp
 
-# Download the path-from-root branch of Automerge locally
-echo "Installing automerge locally ..."
-git clone -b path-from-root --single-branch git@github.com:humandx/automerge.git
-cd automerge
-git pull
-yarn install
-yarn build
-cd ..
+# # Download the path-from-root branch of Automerge locally
+# echo "Installing automerge locally ..."
+# git clone -b path-from-root --single-branch git@github.com:humandx/automerge.git
+# cd automerge
+# git pull
+# yarn install
+# yarn build
+# cd ..
 
 # Install all normal dependencies
-cd ..
+# cd ..
 yarn install
